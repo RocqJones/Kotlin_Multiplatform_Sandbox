f625fe86f8312a44444944d413318abb3f1162be
RocqJones <jonesmbindyo@gmail.com>
2024-01-17 19:10:14 +0300

    Updated Android: Compose

--- shared/src/commonMain/kotlin/com/example/kotlinmultiplatformsandbox/Greeting.kt
+++ shared/src/commonMain/kotlin/com/example/kotlinmultiplatformsandbox/Greeting.kt
@@ -5,11 +5,8 @@ import kotlin.random.Random
 class Greeting {
     private val platform: Platform = getPlatform()
 
-    fun greet(): String {
-        val firstWord  = when {
-            Random.nextBoolean() -> "Hi!"
-            else -> "Hello!"
-        }
-        return "$firstWord Guess what this is! > ${platform.name}!"
+    fun greet(): List<String> = buildList {
+        add(when { Random.nextBoolean() -> "Hi!" else -> "Hello!" })
+        add("Guess what this is! > ${platform.name}!")
     }
 }
