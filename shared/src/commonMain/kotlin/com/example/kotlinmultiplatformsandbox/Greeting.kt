b658c1b1907b3bf46da2b36041e177a1b279c53d
RocqJones <jonesmbindyo@gmail.com>
2024-01-17 18:58:11 +0300

    print platform Name

--- shared/src/commonMain/kotlin/com/example/kotlinmultiplatformsandbox/Greeting.kt
+++ shared/src/commonMain/kotlin/com/example/kotlinmultiplatformsandbox/Greeting.kt
@@ -1,9 +1,15 @@
 package com.example.kotlinmultiplatformsandbox
 
+import kotlin.random.Random
+
 class Greeting {
     private val platform: Platform = getPlatform()
 
     fun greet(): String {
-        return "Hello, ${platform.name}!"
+        val firstWord  = when {
+            Random.nextBoolean() -> "Hi!"
+            else -> "Hello!"
+        }
+        return "$firstWord Guess what this is! > ${platform.name}!"
     }
 }
