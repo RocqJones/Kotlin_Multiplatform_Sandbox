4bca415f4503da9679d2ec8dc1e0bb83ebb965ea
RocqJones <jonesmbindyo@gmail.com>
2024-01-18 16:21:40 +0300

    add new year line to the list

--- shared/src/commonMain/kotlin/com/example/kotlinmultiplatformsandbox/Greeting.kt
+++ shared/src/commonMain/kotlin/com/example/kotlinmultiplatformsandbox/Greeting.kt
@@ -1,5 +1,6 @@
 package com.example.kotlinmultiplatformsandbox
 
+import daysPhrase
 import kotlin.random.Random
 
 class Greeting {
@@ -8,5 +9,6 @@ class Greeting {
     fun greet(): List<String> = buildList {
         add(when { Random.nextBoolean() -> "Hi!" else -> "Hello!" })
         add("Guess what this is! > ${platform.name}!")
+        add(daysPhrase())
     }
 }
