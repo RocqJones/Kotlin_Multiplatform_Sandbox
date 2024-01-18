8d9e0bffe1e1464bf6a5971d28d884636090bf39
RocqJones <jonesmbindyo@gmail.com>
2024-01-18 16:21:22 +0300

    add external dependencies: ktor & coroutines

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
+    kotlin("plugin.serialization") version "1.9.21"
 }
 
 kotlin {
@@ -23,11 +24,31 @@ kotlin {
         }
     }
 
+    val ktorVersion = "2.3.5"
+
     sourceSets {
         commonMain.dependencies {
             //put your multiplatform dependencies here
             implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
+
+            // kotlinx.coroutines
+            implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+
+            //Ktor
+            implementation("io.ktor:ktor-client-core:$ktorVersion")
+            implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
+            implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
         }
+
+        // Provide the platform engines by adding dependencies on the corresponding artifacts in the platform source sets
+        androidMain.dependencies {
+            implementation("io.ktor:ktor-client-android:$ktorVersion")
+        }
+
+        iosMain.dependencies {
+            implementation("io.ktor:ktor-client-darwin:$ktorVersion")
+        }
+
         commonTest.dependencies {
             implementation(libs.kotlin.test)
         }
